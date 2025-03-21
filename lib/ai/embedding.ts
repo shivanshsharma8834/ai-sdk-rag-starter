365d13233eaec86d1a5edc49cc60a19900609937
Shivansh Sharma <shivansh.report@gmail.com>
2025-03-21 20:12:12 +0530

    added neondb vector embedding

---
+++ lib/ai/embedding.ts
@@ -0,0 +1,27 @@
+import {embedMany} from 'ai';
+import { groq } from '@ai-sdk/groq'
+import { google } from '@ai-sdk/google'
+
+const embeddingModel = google.textEmbeddingModel('text-embedding-004');
+
+const generateChunks = (input: string): string[] => {
+    return input
+    .trim()
+    .split('.')
+    .filter(i => i !== '')
+}
+
+export const generateEmbeddings = async (
+    value: string,
+): Promise<Array<{embedding: number[]; content: string}>> => {
+    const chunks = generateChunks(value);
+    const { embeddings } = await embedMany({
+        model: embeddingModel,
+        values: chunks
+    });
+
+    return embeddings.map((e, i) => ({
+        content: chunks[i],
+        embedding: e
+    }))
+}
---
+++ lib/db/schema/embeddings.ts
@@ -0,0 +1,25 @@
+import { nanoid } from "nanoid";
+import { index, pgTable, text, varchar, vector,   } from "drizzle-orm/pg-core";
+import { resources } from "./resources";
+
+export const embeddings = pgTable(
+    'embeddings',
+    {
+        id: varchar('id', {length: 191})
+        .primaryKey()
+        .$defaultFn(() => nanoid()),
+        resourceId: varchar('resourceId', {length: 191}).references(
+            () => resources.id,
+            {onDelete: 'cascade'}
+        ),
+        content: text('content').notNull(),
+        embedding: vector('embedding', {dimensions: 768}).notNull(),
+
+    },
+    table => ({
+        embeddingIndex: index('embeddingIndex').using(
+            'hnsw',
+            table.embedding.op('vector_cosine_ops')
+        )
+    })
+)
